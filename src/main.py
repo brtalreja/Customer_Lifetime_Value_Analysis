f8182747e615133114c72469172461aa125f896d
brtalreja <110854777+brtalreja@users.noreply.github.com>
2024-08-01 02:35:49 -0500

    Most and Least profitable channels

--- src/main.py
+++ src/main.py
@@ -34,3 +34,23 @@ fig.write_image("../output/distribution_of_revenue.png")
 # To increase revenue, the middle revenue ranges should be focused as those are more common and can help target marketing and sales strategies effectively.
 # The lower and higher extremes might be areas to explore further to understand why they are less frequent.
 
+#Most and Least profitable channels
+cost_by_channel = data.groupby('channel')['cost'].mean().reset_index()
+
+fig = px.bar(cost_by_channel,
+            x = "channel",
+            y = "cost",
+            title = "Customer Acquisition Cost by Channel",
+            labels = {"cost": "Acquisition cost", "channel": "Channel"})
+
+fig.show()
+
+fig.write_image("../output/customer_acquistion_cost_by_channel.png")
+
+#COMMENT: Email Marketing has the lowest CAC at 5.25 (the most cost-effective channel for acquiring customers).
+# Paid Advertising has the highest CAC at 30.45 (the most expensive channel for acquiring customers).
+# If the organization's goal is to minimize CAC, more budget should be allocated to email marketing.
+# However, if the budget is not a problem, strategies should be reassesed to reduce the high CAC for paid advertising.
+# Because the email marketing is driving significant customer acquisition at a low cost, it could be a key area to expand.
+# Evaluate Referral and Social Media channels to assess their performance and determine if their CAC can be reduced or if they offer any other strategic benefits.
+
